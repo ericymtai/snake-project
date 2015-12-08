052be61b2507a6534d49b248735ec67b26a482f3
ericytai <ericym2@gmail.com>
2015-12-07 22:30:22 -0500

    stop music when pause

--- snake3.js
+++ snake3.js
@@ -18,10 +18,10 @@ $(document).ready(function () {
 	// set variables for button 1 payer and 2 players
 	var button = document.getElementById("btn");
 
-	// set variable for button restart
+	// set variable for button back
 	var button3 = document.getElementById("btn3");
 
-	// set variable for button restart
+	// set variable for button pause
 	var button4 = document.getElementById("btn4");
 
 
@@ -29,7 +29,7 @@ $(document).ready(function () {
 	var canvasBoard = document.getElementById("canvas");
 
 	// set variable for the div that includes 
-	var board = document.getElementById("board");
+	// var board = document.getElementById("board");
 
 	// set variable for getting five foods
 	var congratulation = document.getElementById("congratulation");
@@ -58,8 +58,9 @@ var game_loop;
 
 	// create the stage for goldsnake
 	function goldsnake() {
-		board.style.display = "none";		// hide select players' buttons
+		button.style.display = "none";		// hide select players' buttons
 		button3.style.display = "block";	// show restart button
+		button4.style.display = "block";
 		d = "left";		// goldsnake's default direction
 		create_goldsnake();	// now we can see the goldsnake			
 		create_food(); 	// now we can see the food particle	for goldsnake
@@ -73,7 +74,7 @@ var game_loop;
 		// let's move the goldsnake now using a timer which will trigger the paint function every 90ms
 		if (typeof game_loop != "undefined") clearInterval(game_loop);
 		// game_loop = setInterval(paint, 120);
-		reStart();
+		reStart(200);
 	}
 
 	// back to the beginning stage when press restart button
@@ -98,17 +99,20 @@ var game_loop;
 	}
 	// let's set to pause the game
 	function pause() {
+
 		if(game_loop!=null){
 			clearInterval(game_loop);
+			crashSoundStop();
+			bgMusicStop();
 			game_loop=null;
 			console.log('print pause');
 		} else {
-			reStart();
+			reStart(200);
 	    }
 	}
 	// let's set to re-start the game
-	function reStart() {
-		game_loop = setInterval(paint, 120);
+	function reStart(b) {
+		game_loop = setInterval(paint, b);
 		console.log('print restart');
 	}
 
@@ -144,6 +148,16 @@ var game_loop;
 		audio3.play();		// sound effect when the snake hits walls 
 		audio3.volume = .2;	// turn down the sound volume
 	}
+	function crashSoundStop() {
+		audio3.stop();		// sound effect when the snake hits walls 
+	}
+	function bgMusic() {
+		audio2.play();		// sound effect when the snake hits walls 
+		audio3.volume = .2;	// turn down the sound volume
+	}
+	function bgMusicStop() {
+		audio2.pause();		// sound effect when the snake hits walls 
+	}
 
 	// let's create the re-used function for the goldsnake
 	function goldSnakeMove() {
@@ -176,25 +190,28 @@ var game_loop;
 		if(nx == food.x && ny == food.y) {
 			var tail = {x: nx, y: ny};
 			score++;			// add one when snake eat food
-			audio2.play();		// sound effect when snake eat food
+			// audio2.play();		// sound effect when snake eat food
+			bgMusic();
 			create_food();		// create new food	
 
 			if (score==0) {
-				addSpeed(120);
+				reStart(190);
 			} else if (score==1) {
-				addSpeed(100);
+				reStart(180);
 			} else if (score==2) {
-				addSpeed(80);
+				reStart(170);
 			} else if (score==3) {
-				addSpeed(60);
+				reStart(160);
 			} else if (score==4) {
-				addSpeed(40);
+				reStart(150);
 			} else if (score==5) {
 				congratulation.style.display = "block";
 				canvasBoard.style.display = "none";
-				board.style.display = "none";		// hide select players' buttons
-				button3.style.display = "none";
+				button.style.display = "none";		// hide select players' buttons
+				button3.style.display = "block";
 				button4.style.display = "none";
+				crashSoundStop();
+				bgMusicStop();
 			}
 		}  else {
 			var tail = goldsnake_array.pop(); // pops out the last xell
